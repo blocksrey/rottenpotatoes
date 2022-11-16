8418f2ad145afcd73c9caae7ba6c660668d132c3
Daniela B. Fajardo <danielafajardo@ewhain.net>
2022-11-16 21:38:31 +0900

    Update connection.php
    Changed connection's username and password according to the ones we will use for the migration process.

--- components/connection.php
+++ components/connection.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "team22";
+    $password = "team22";
     $dbname = "team22";
 
     $conn = mysqli_connect($host, $username, $password, $dbname);
